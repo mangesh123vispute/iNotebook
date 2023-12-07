5465b89e30d3cc7f4ba8e0cec8ae1ee1e32fff3b
mangesh vispute <mangesh2003vispute@gmail.com>
2023-12-07 10:22:21 +0530

    added end points , added crud operation on the notes and authentication is applied

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const port = 5000;
 
 app.use("/api/auth", require("./routes/auth"));
-// app.use("/api/notes", require("./routes/notes"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
 const JWT_SECRET = "Mangeshisgood$boy";
+const fetchuser = require("../middelware/fetchuser");
+
+// Route 1:create a user using POST:"/api/auth/createuser" no login required
 
 router.post(
   "/createuser",
@@ -16,33 +19,90 @@ router.post(
     }),
   ],
   async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ errors: "User already exists" });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      var authToken = jwt.sign(data, JWT_SECRET);
+
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+// Route 2:Authenticating user using POST:"/api/auth/login"  login required
+router.post(
+  "/login",
+  [
+    body("email", "Email must be valid email").isEmail(),
+    body("password", "password should be provided").exists(),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-
-    let user = await User.findOne({ email: req.body.email });
-    if (user) {
-      return res.status(400).json({ errors: "User already exists" });
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: "Please try to login with correct credentials" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ errors: "Please try to login with correct credentials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Internal Server Error");
     }
-
-    const salt = await bcrypt.genSalt(10);
-    const secPass = await bcrypt.hash(req.body.password, salt);
-
-    user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: secPass,
-    });
-    const data = {
-      user: {
-        id: user.id,
-      },
-    };
-    var authToken = jwt.sign(data, JWT_SECRET);
-    console.log(jwtdata);
-    res.json({ authToken });
   }
 );
+module.exports = router;
 
+// ROUTE 3: Get loggedin User Details using: POST "/api/auth/getuser". Login required
+router.post("/getuser", fetchuser, async (req, res) => {
+  try {
+    userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
 module.exports = router;
