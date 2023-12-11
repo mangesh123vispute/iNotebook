e43c8dacf49091e5fb813cd4a6647afc2bec274e
mangesh vispute <mangesh2003vispute@gmail.com>
2023-12-11 12:45:49 +0530

    added allowed origin

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const connectToMongo = require("./db");
-var cors = require("cors");
-var app = express();
+const cors = require("cors");
+const app = express();
 
-app.use(cors());
+// Specify the allowed origin (your Netlify domain)
+const allowedOrigin = "https://famous-sfogliatella-ad481a.netlify.app";
+
+// Configure CORS with custom options
+const corsOptions = {
+  origin: allowedOrigin,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Enable credentials (e.g., cookies)
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 connectToMongo();
 
 app.use(express.json());
-const port = 5000;
+const port = 5000; // Use the default port
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
